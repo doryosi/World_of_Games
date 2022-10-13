833e586cfd796cb8826d1a107f896daa4e0328d7
doryosi <dorsinai1004@gmail.com>
2022-10-13 19:10:15 +0300

    Commit

--- e2e.py
+++ e2e.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 import argparse
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,46 +1,31 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdrivermanager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options
-import argparse
-from Utils import bad_return_code, success_return_code
-
-DEFAULT_FLASK_APP_URL = "http://127.0.0.1:5003/"
-BIN_CHROME_DRIVER_DEST = "/usr/local/bin"
 
 
-def test_scores_service(url):
+# Grabbing the chromedriver in order to do tests
+def safe_cast(val, to_type, default=int):
     try:
-        paths = []
-        chrome_options = Options()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        full_bin_path = ChromeDriverManager(path=BIN_CHROME_DRIVER_DEST).install()
-        my_driver = webdriver.Chrome(service=ChromeService(full_bin_path), options=chrome_options)
-        my_driver.get(url)
-        score = my_driver.find_element(by="xpath", value='//*[@id="score"]').text
-        print(score)
-        my_driver.close()
-        return int(score) in range(1, 1001)
-    except ValueError as e:
-        print(e)
-        return False
+        return to_type(val)
+    except (ValueError, TypeError):
+        return default
 
 
-def main_function(url):
-    if test_scores_service(url):
-        return success_return_code
+def test_scores_service():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().download_and_install()))
+    # driver = webdriver.Chrome(executable_path="C:/chromedriver")
+    driver.get("http://127.0.0.1:5003")
+    wait = WebDriverWait(driver, 20)
+    score = int(wait.until(EC.visibility_of_element_located((By.ID, "score"))).text)
+    if 1000 >= score >= 1:
+        return exit(0)
     else:
-        print("Score is not in range...")
-        return bad_return_code
+        return exit(-1)
 
 
-parser = argparse.ArgumentParser(description='End to end testing',
-                                 formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument('-u', action='store', dest='flask_url', default=DEFAULT_FLASK_APP_URL, type=str,
-                    help=f"Flask server url e.g. {DEFAULT_FLASK_APP_URL}")
-args = parser.parse_args()
-flask_url = args.flask_url
-if main_function(flask_url) < 0:
-    exit(1)
+test_scores_service()
+
+
